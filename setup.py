bcf17646c807b0134cf4a3bf177aa49bab9901c3
Lucien Haurat <lucien.haurat@free.fr>
2018-07-21 02:38:42 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='revfyawo.mongo',
+    version='0.0.1',
+    description='A mongo ODM',
+    url='https://gitlab.com/revfyawo/mongo',
+    author='Lucien Haurat',
+    author_email='lucien.haurat@gmail.com',
+    packages=['revfyawo.mongo'],
+    install_requires=['pymongo~=3.7'],
+)
+
