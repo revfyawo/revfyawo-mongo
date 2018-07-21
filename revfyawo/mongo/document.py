47e8e8ba8451a33764e4dedf5a770891824c3295
Lucien Haurat <lucien.haurat@free.fr>
2018-07-21 13:03:06 +0200

    moved client db and collection to Meta class

--- revfyawo/mongo/document.py
+++ revfyawo/mongo/document.py
@@ -4,35 +4,22 @@ from bson import ObjectId
 from pymongo import MongoClient
 
 
-not_fields = ('_client', '_db', '_collection')
-
-
 class Document:
-    _client: Optional[MongoClient] = None
-    _db: str = ''
-    _collection: str = __name__.lower()
-
     _id: Optional[ObjectId] = None
 
     class Meta:
+        client: Optional[MongoClient] = None
+        db: str = ''
+        collection: str = __name__.lower()
         fields = []
-
-    @classmethod
-    def set_client(cls, client: MongoClient):
-        cls._client = client
-
-    @classmethod
-    def set_db_name(cls, db_name: str):
-        cls._db = db_name
 
     @property
     def id(self) -> Optional[ObjectId]:
         return self._document.get('_id')
 
     def __init__(self, **kwargs):
+        self.Meta.fields = get_type_hints(self.__class__)
         self._document = kwargs
-        type_hints = get_type_hints(self.__class__)
-        self.Meta.fields = list(filter(lambda x: x not in not_fields, type_hints))
 
     def __getattr__(self, item):
         if item in self.Meta.fields:
@@ -46,7 +33,7 @@ class Document:
             super(Document, self).__setattr__(key, value)
 
     def insert(self):
-        return self._client[self._db][self._collection].insert_one(self._document)
+        return self.Meta.client[self.Meta.db][self.Meta.collection].insert_one(self._document)
 
     @classmethod
     def one(cls, filter_=None):
