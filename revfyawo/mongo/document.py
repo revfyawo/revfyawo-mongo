cea87defff4ca7cd86f8347367f1f775109b75da
Lucien Haurat <lucien.haurat@free.fr>
2018-07-21 18:13:15 +0200

    moved Meta.fields to __fields & added get_collection

--- revfyawo/mongo/document.py
+++ revfyawo/mongo/document.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Optional, get_type_hints, List
 
 from bson import ObjectId
@@ -8,15 +9,11 @@ not_fields = ('_client', '_db', '_collection')
 
 
 class Document:
-    _id: Optional[ObjectId] = None
+    _id: Optional[ObjectId]
 
-    _client: Optional[MongoClient] = None
-    _db: str = ''
-    _collection: str = ''
-
-    class Meta:
-        collection: Optional[Collection] = None
-        fields = []
+    _client: MongoClient
+    _db: str
+    _collection: str
 
     @property
     def id(self) -> Optional[ObjectId]:
@@ -28,16 +25,19 @@ class Document:
 
     def __init__(self, **kwargs):
         hints = get_type_hints(self.__class__)
-        self.Meta.fields = list(filter(lambda x: x not in not_fields, hints))
+        self.__fields = list(filter(lambda x: x not in not_fields, hints))
+        self.connect(self._client, self._db, self._collection)
         self._document = kwargs
 
     def __getattr__(self, item):
-        if item in self.Meta.fields:
+        if item in self.__fields:
             return self._document.get(item)
         raise AttributeError
 
     def __setattr__(self, key, value):
-        if key in self.Meta.fields:
+        if key == '_Document__fields':
+            super(Document, self).__setattr__(key, value)
+        if key in self.__fields:
             self._document[key] = value
         else:
             super(Document, self).__setattr__(key, value)
@@ -52,27 +52,28 @@ class Document:
         if collection:
             cls._collection = collection
 
-        if not cls._collection:
+        if not hasattr(cls, '_collection'):
             cls._collection = cls.__name__.lower()
 
-        if cls._client and cls._db and cls._collection:
-            cls.Meta.collection = cls._client[cls._db][cls._collection]
+    @classmethod
+    def _get_collection(cls) -> Collection:
+        return cls._client[cls._db][cls._collection]
 
     def insert(self):
-        return self.Meta.collection.insert_one(self._document)
+        return self._get_collection().insert_one(self._document)
 
     @classmethod
     def one(cls, filter_=None) -> Optional['Document']:
-        doc = cls.Meta.collection.find_one(filter_)
+        doc = cls._get_collection().find_one(filter_)
         return cls(**doc) if doc else None
 
     @classmethod
     def many(cls, filter_=None) -> List['Document']:
-        docs = cls.Meta.collection.find(filter_)
+        docs = cls._get_collection().find(filter_)
         return [cls(**doc) for doc in docs]
 
     def update(self):
-        return self.Meta.collection.find_one_and_replace({'_id': self.id}, self._document)
+        return self._get_collection().find_one_and_replace({'_id': self.id}, self._document)
 
     def delete(self):
-        return self.Meta.collection.find_one_and_delete({'_id': self.id})
+        return self._get_collection().find_one_and_delete({'_id': self.id})
