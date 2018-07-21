4eb78995fa850580d8f9cdc041f52ec8924f31c0
Lucien Haurat <lucien.haurat@free.fr>
2018-07-21 14:31:07 +0200

    back to _client _db and _collection

--- revfyawo/mongo/document.py
+++ revfyawo/mongo/document.py
@@ -2,15 +2,20 @@ from typing import Optional, get_type_hints
 
 from bson import ObjectId
 from pymongo import MongoClient
+from pymongo.collection import Collection
+
+not_fields = ('_client', '_db', '_collection')
 
 
 class Document:
     _id: Optional[ObjectId] = None
 
+    _client: Optional[MongoClient] = None
+    _db: str = ''
+    _collection: str = ''
+
     class Meta:
-        client: Optional[MongoClient] = None
-        db: str = ''
-        collection: str = __name__.lower()
+        collection: Optional[Collection] = None
         fields = []
 
     @property
@@ -18,7 +23,8 @@ class Document:
         return self._document.get('_id')
 
     def __init__(self, **kwargs):
-        self.Meta.fields = get_type_hints(self.__class__)
+        hints = get_type_hints(self.__class__)
+        self.Meta.fields = list(filter(lambda x: x not in not_fields, hints))
         self._document = kwargs
 
     def __getattr__(self, item):
@@ -32,13 +38,29 @@ class Document:
         else:
             super(Document, self).__setattr__(key, value)
 
+    @classmethod
+    def connect(cls, client: Optional[MongoClient] = None,
+                db: str = '', collection: str = ''):
+        if client:
+            cls._client = client
+        if db:
+            cls._db = db
+        if collection:
+            cls._collection = collection
+
+        if not cls._collection:
+            cls._collection = cls.__name__.lower()
+
+        if cls._client and cls._db and cls._collection:
+            cls.Meta.collection = cls._client[cls._db][cls._collection]
+
     def insert(self):
-        return self.Meta.client[self.Meta.db][self.Meta.collection].insert_one(self._document)
+        return self.Meta.collection.insert_one(self._document)
 
     @classmethod
     def find_one(cls, filter_=None) -> Optional['Document']:
-        doc = cls.Meta.client[cls.Meta.db][cls.Meta.collection].find_one(filter_)
+        doc = cls.Meta.collection.find_one(filter_)
         return cls(**doc) if doc else None
 
     def delete(self):
-        return self.Meta.client[self.Meta.db][self.Meta.collection].find_one_and_delete({'_id': self.id})
+        return self.Meta.collection.find_one_and_delete({'_id': self.id})
