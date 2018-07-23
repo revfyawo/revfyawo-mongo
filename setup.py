4d853eda3280eec12c58409e68de380d69b575c7
Lucien Haurat <lucien.haurat@free.fr>
2018-07-23 21:05:36 +0200

    version bump 0.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='revfyawo.mongo',
-    version='0.0.3',
+    version='0.0.4',
     description='A mongo ODM',
     url='https://gitlab.com/revfyawo/mongo',
     author='Lucien Haurat',
