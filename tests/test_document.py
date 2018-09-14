3518028ac2aac9cfe18ecaec8b08756d49e5b584
Lucien Haurat <lucien.haurat@free.fr>
2018-09-14 19:25:00 +0200

    add test document fields

--- tests/test_document.py
+++ tests/test_document.py
@@ -1,6 +1,6 @@
 import pytest
 
-from tests.models import Author
+from tests.models import Author, author_fields
 
 
 def test_create_kwargs(author_dict):
@@ -18,6 +18,11 @@ def test_create_document(author_dict):
 def test_create_bot_kwargs_document(author_dict):
     with pytest.raises(AttributeError):
         author = Author(author_dict, **author_dict)
+
+
+def test_correct_fields():
+    author = Author()
+    assert getattr(author, '_Document__fields') == ['_id'] + list(author_fields.keys())
 
 
 def test_setattr(author_dict):
