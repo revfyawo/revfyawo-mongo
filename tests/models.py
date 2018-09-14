ef66e2265c1aae4846bcd53bb6f553be63851ff1
Lucien Haurat <lucien.haurat@free.fr>
2018-09-14 18:32:02 +0200

    removed unused test author fields & added field dicts

--- tests/models.py
+++ tests/models.py
@@ -3,13 +3,27 @@ from typing import List
 
 from revfyawo.mongo import Document
 
+author_fields = {
+    'username': str,
+    'password': str,
+    'articles': List[str],
+    'created_at': datetime.datetime
+}
+
+article_fields = {
+    'author': str,
+    'slug': str,
+    'title': str,
+    'content': str,
+    'seen': int,
+    'created_at': datetime.datetime,
+    'updated_at': datetime.datetime
+}
+
 
 class Author(Document):
     username: str
     password: str
-    email: str
-    name: str
-    address: str
     articles: List[str]
     created_at: datetime.datetime
 
