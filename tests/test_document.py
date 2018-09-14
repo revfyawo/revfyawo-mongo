34dcef4e7d0d4b7fdf6ba63fd6b8668a7cd378a2
Lucien Haurat <lucien.haurat@free.fr>
2018-09-14 18:23:45 +0200

    test create with kwargs, dict, and both

--- tests/test_document.py
+++ tests/test_document.py
@@ -1,4 +1,4 @@
-import datetime
+import pytest
 
 from revfyawo.mongo import Document
 from tests.conftest import db_client, db_name
@@ -13,12 +13,21 @@ def teardown_module():
     db_client.drop_database(db_name)
 
 
-def test_create(author_dict):
+def test_create_kwargs(author_dict):
     author = Author(**author_dict)
-    assert isinstance(author.username, str)
-    assert isinstance(author.password, str)
-    assert isinstance(author.created_at, datetime.datetime)
+    assert author.document == author_dict
     assert author.id is None
+
+
+def test_create_document(author_dict):
+    author = Author(author_dict)
+    assert author.document == author_dict
+    assert author.id is None
+
+
+def test_create_bot_kwargs_document(author_dict):
+    with pytest.raises(AttributeError):
+        author = Author(author_dict, **author_dict)
 
 
 def test_setattr(author_dict):
