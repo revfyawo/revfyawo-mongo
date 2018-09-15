04e8e7a0800740f81d5fff374313880fe153c875
Lucien Haurat <lucien.haurat@free.fr>
2018-09-15 04:14:16 +0200

    subdocuments start

---
+++ revfyawo/mongo/subdocument.py
@@ -0,0 +1,2 @@
+class SubDocument(dict):
+    pass
--- tests/models.py
+++ tests/models.py
@@ -1,29 +1,29 @@
 import datetime
 from typing import List
 
-from revfyawo.mongo import Document
+from revfyawo.mongo import Document, SubDocument
 
-author_fields = {
-    'username': str,
-    'password': str,
-    'articles': List[str],
-    'created_at': datetime.datetime
-}
 
-article_fields = {
-    'author': str,
-    'slug': str,
-    'title': str,
-    'content': str,
-    'seen': int,
-    'created_at': datetime.datetime,
-    'updated_at': datetime.datetime
-}
+class BankInfo(SubDocument):
+    iban: str
+    credit_card: int
+    credit_card_code: int
+    credit_card_owner: str
+
+
+class UserInfo(SubDocument):
+    address: str
+    bank_info: BankInfo
+    description: str
+    email: str
+    job: str
+    phone_number: str
 
 
 class Author(Document):
     username: str
     password: str
+    user_info: UserInfo
     articles: List[str]
     created_at: datetime.datetime
 
@@ -40,3 +40,22 @@ class Article(Document):
     updated_at: datetime.datetime
 
     _collection = 'articles'
+
+
+author_fields = {
+    'username': str,
+    'password': str,
+    'user_info': UserInfo,
+    'articles': List[str],
+    'created_at': datetime.datetime
+}
+
+article_fields = {
+    'author': str,
+    'slug': str,
+    'title': str,
+    'content': str,
+    'seen': int,
+    'created_at': datetime.datetime,
+    'updated_at': datetime.datetime
+}
